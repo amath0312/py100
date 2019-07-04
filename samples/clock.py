b9745b2d421c2b92bb2ebbd938209b79f1bea1d8
amath0312 <amath0312@gmail.com>
2019-07-04 12:26:30 +0800

    优化计时器

--- samples/clock.py
+++ samples/clock.py
@@ -14,6 +14,7 @@ class Clock(object):
         self._max = -1
         self._thread = None
         self._running = False
+        self._render_template = '00:00:00 .000'
 
     @property
     def render(self):
@@ -24,6 +25,13 @@ class Clock(object):
         self._render = render
 
     def stop(self):
+        if self._render:
+            self._render(self._render_template)
+        else:
+            os.system('clear')
+            print(self._render_template)
+
+        self._render_template = '00:00:00 .000'
         self._running = False
         self._thread = None
 
@@ -37,24 +45,41 @@ class Clock(object):
         self._running = True
         self._thread.start()
 
+    def rend_timer(self, start, end):
+        ''' 计算计时时间并渲染计时器 '''
+        if not self._running:
+            return 
+        hour, minute, second, ms = Clock._parse_time(start, end)
+        self._rend(hour, minute, second, ms)        
+
     @staticmethod
     def _timer_tick(clock):
+        ''' 定时刷新计时器 '''
         start = time.time()
         while clock._running and (clock._max == -1 or (time.time() - start) <= clock._max):
             time.sleep(0.1)
-            render_tmpl = Clock._rend_timer(start, time.time())
-            if clock._render:
-                clock._render(render_tmpl)
-            else:
-                os.system('clear')
-                print(render_tmpl)
+            render_tmpl = clock.rend_timer(start, time.time())
+            
         clock._running = False
         clock._thread = None
 
-    @staticmethod
-    def _rend_timer(start, end):
-        dur = int((end - start) // 1)
+    def _rend(self, hour, minute, second, ms):
+        ''' 渲染计时器 '''
+        self._render_template = '%02d:%02d:%02d .%03d' % (hour, minute, second, ms)
+        if self._render:
+            self._render(self._render_template)
+        else:
+            os.system('clear')
+            print(self._render_template)
 
+    @staticmethod
+    def _parse_time(start, end):
+        ''' 计算出计时器当前计时（时、分、秒、毫秒） '''
+        dur = int((end - start) * 1000 // 1)
+
+        ms = int(dur % 1000)
+
+        dur = dur // 1000
         second = dur % 60
 
         dur = dur // 60
@@ -62,9 +87,7 @@ class Clock(object):
 
         dur = dur // 60
         hour = dur
-
-        return '%02d:%02d:%02d' % (hour, minute, second)
-
+        return hour, minute, second, ms
 
 def listen():
     while(True):
@@ -72,6 +95,7 @@ def listen():
         if action.lower() == 'q':
             break
     sys.exit(0)
+
 
 def clock_term(max=-1):
     clock = Clock()
@@ -86,8 +110,8 @@ def clock_gui():
     root.title('计时器')
     ws = root.winfo_screenwidth()
     hs = root.winfo_screenheight()
-    w = 200
-    h = 200
+    w = 300
+    h = 300
     x = (ws / 2) - (w / 2)
     y = (hs / 2) - (h / 2)
     root.geometry('%dx%d+%d+%d' % (w, h, x, y))
@@ -95,15 +119,15 @@ def clock_gui():
 
     tkinter.Label(root, height=2).pack(side='top')
 
-    label = tkinter.Label(root, text='00:00:00', height=3,
-                          font='"Source Han Sans SC Light" -24 underline', anchor='center')
+    label = tkinter.Label(root, text='00:00:00 .000', height=3,
+                          font='"Source Han Sans SC Light" -42 underline', anchor='center')
     label.pack()
 
     panel = tkinter.Frame(root)
     tkinter.Button(panel, text='启动', font='"Source Han Sans SC Light" -16',
-                   command=clock.start).pack(side='left')
+                   command=clock.start).pack(side='left', padx=10)
     tkinter.Button(panel, text='停止', font='"Source Han Sans SC Light" -16',
-                   command=clock.stop).pack(side='right')
+                   command=clock.stop).pack(side='right', padx=10)
     panel.pack()
     tkinter.Label(root).pack(side='bottom')
 
