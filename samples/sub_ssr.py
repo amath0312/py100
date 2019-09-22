9d23cbb026b49b800a155ac961eb2e6d3319fecd
amath0312 <amath0312@gmail.com>
2019-09-22 12:54:10 +0800

    handle b64

--- samples/sub_ssr.py
+++ samples/sub_ssr.py
@@ -14,13 +14,35 @@ def get_sub_ssr(url):
     content = safe_b64decode(b64content).decode()
     return content.split()
 
-def repack(ssr_list):
+def repack(ssr_list, b64_fmt=False):
     repack_list = []
     for ssr in ssr_list:
         param = safe_b64decode(ssr[6:]).decode()+'&protoparam=&obfsparam='
-        repack_ssr = 'ssr://'+base64.b64encode(param.encode()).decode()
+        param = safe_param(param)
+        repack_ssr = ''
+        if b64_fmt:
+            repack_ssr = 'ssr://'+base64.b64encode(param.encode()).decode()
+        else:
+            repack_ssr = 'ssr://'+param
         repack_list.append(repack_ssr)
     return repack_list
+
+def safe_param(param):
+    host, port, fmt, encmode, confuse, pwd_and_others = param.split(':')
+    
+    pwd = pwd_and_others[0:pwd_and_others.index('/?')]
+    pwd = pwd.rstrip('=')
+    queries = pwd_and_others[pwd_and_others.index('/?')+2:]
+    simple_queries = 'group='+base64.b64encode('st-link'.encode()).decode().rstrip('=')
+    for entry in queries.split('&'):
+        key = entry[:entry.index('=')]
+        value = entry[entry.index('=')+1:]
+        value = value.rstrip('=')
+        simple_queries = simple_queries + "&" + key+"="+value
+
+
+    return host+':'+port+':'+fmt+':'+encmode+':'+confuse+':'+pwd+'/?'+simple_queries
+
 
 def subscribe(url):
     ssr_list = get_sub_ssr(url)
@@ -28,10 +50,19 @@ def subscribe(url):
     repack_content = '\n'.join(repack_list)
     return base64.b64encode(repack_content.encode()).decode()
 
+def subscribe_simple_fmt(url):
+    ssr_list = get_sub_ssr(sub_url)
+    repack_list = repack(ssr_list, b64_fmt=False)
+    return '\n\n'.join(repack_list)
+
+def subscribe_b64_fmt(url):
+    ssr_list = get_sub_ssr(sub_url)
+    repack_list = repack(ssr_list, b64_fmt=True)
+    return '\n\n'.join(repack_list)
+
 if __name__ == '__main__':
     # sub_url = sys.argv[1]
     sub_url = 'https://api.st.link/sss/nasa/c3269c34'
-    ssr_list = get_sub_ssr(sub_url)
-    repack_list = repack(ssr_list)
-    print('\n\n'.join(repack_list))
+    print(subscribe_b64_fmt(sub_url))
+
     
